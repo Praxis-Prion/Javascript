5fc4e65e3e03912b344a8f26ba6c9a118c7d6a7a
Praxis <34970798+Praxis-Prion@users.noreply.github.com>
2024-08-05 22:55:33 -0500

    Added content to variablesAndOperators.js
    Starting on dataTypesAndConditionals.js

---
+++ dataTypesAndConditionals.js
@@ -0,0 +1,20 @@
+// COMPARISON OPERATORS
+
+// Strict equality
+k = 10;
+console.log(k === 2 + 3);
+
+// Strict-non-equality
+console.log(k !== 2 + 3);
+
+// Less than
+console.log(k < 5);
+
+// Greater than
+console.log(k > 5);
+
+// Less than or equal to
+console.log(k <= 10);
+
+// Greater than or equal to
+console.log(k >= 20);
--- variablesAndOperators.js
+++ variablesAndOperators.js
@@ -22,6 +22,8 @@ f = "ff";
 let g = "g";
 let h = g;
 
+console.log(a, b, c, d, e, f, g, h);
+
 ////////////////////////////////////////////////////////////////////////////////
 // CONSTANTS
 ////////////////////////////////////////////////////////////////////////////////
@@ -34,4 +36,54 @@ const i = "i";
 // are named using capital letters and underscores.
 const COLOR_RED = "#F00";
 
-console.log(a, b, c, d, e, f, g, h, i, COLOR_RED)
+console.log(i, COLOR_RED)
+
+////////////////////////////////////////////////////////////////////////////////
+// OPERATORS
+////////////////////////////////////////////////////////////////////////////////
+
+let addition = 6 + 9;
+let subtraction = 20 - 15;
+let multiplication = 3 * 7;
+let division = 10 / 5;
+let modulo = 8 % 3;
+let exponent = 5 ** 2;
+
+console.log(addition, subtraction, multiplication, division, modulo, exponent)
+
+// Using variables for math
+j = addition + (subtraction - multiplication) * division % modulo ** exponent;
+console.log(j);
+
+// Increment & decrement
+let num1 = 4;
+num1++;
+
+let num2 = 6;
+num2--;
+num2;
+
+console.log(num1, num2)
+
+// Assignment operators +=, -=, *=, /=
+// eg. short for x = x + 4
+let additionAssignment = 4;
+additionAssignment += 4;
+console.log(additionAssignment)
+
+////////////////////////////////////////////////////////////////////////////////
+// USEFUL NUMBER METHODS
+////////////////////////////////////////////////////////////////////////////////
+
+// toFixed() - Round number to fixed decimal
+const lotsOfDecimal = 1.766584958675746364;
+console.log(lotsOfDecimal);
+const twoDecimalPlaces = lotsOfDecimal.toFixed(2);
+console.log(twoDecimalPlaces);
+
+// Number() - Converting to number data types
+let myNumber = "74";
+console.log(typeof(myNumber));
+
+myNumber = Number(myNumber) + 3;
+console.log(typeof(myNumber));
